658c67f2d20c6714bbb3cf499815cf619adcaeb4
ivanbrennan <ivan.brennan@gmail.com>
2020-11-27 17:45:46 -0500

    deploy-to-kind
    nix-shell
    deploy-to-kind
    kubectl --context kind-kind get service hello
    pod=$(kubectl --context=kind-kind get pods -l "app=hello" -o jsonpath="{.items[0].metadata.name}")
    kubectl --context kind-kind port-forward $pod 3000:3000 &
    curl localhost:3000
    fg
    ^C
    kind delete cluster
    exit

---
+++ kind/configuration.nix
@@ -0,0 +1,43 @@
+let
+  kubeVersion = "1.14";
+
+  helloApp = rec {
+    label = "hello";
+    port = 8080;
+    cpu = "100m";
+    imagePolicy = "Never";
+    env = [
+      {
+        name = "APP_PORT";
+        value = toString port;
+      }
+    ];
+  };
+in
+  {
+    kubernetes.version = kubeVersion;
+
+    kubernetes.resources.deployments."${helloApp.label}" = {
+      metadata.labels.app = helloApp.label;
+      spec = {
+        replicas = 1;
+        selector.matchLabels.app = helloApp.label;
+        template = {
+          metadata.labels.app = helloApp.label;
+          spec.containers."${helloApp.label}" = {
+            name = helloApp.label;
+            image = "hello-app:latest";
+            imagePullPolicy = helloApp.imagePolicy;
+            env = helloApp.env;
+            resources.requests.cpu = helloApp.cpu;
+            ports."${toString helloApp.port}" = {};
+          };
+        };
+      };
+    };
+
+    kubernetes.resources.services."${helloApp.label}" = {
+      spec.selector.app = helloApp.label;
+      spec.ports."${toString helloApp.port}".targetPort = helloApp.port;
+    };
+  }
--- kind/default.nix
+++ kind/default.nix
@@ -7,12 +7,25 @@ let
     inherit sha256;
   }) { };
 
+  kubenix = let
+    rev = "611059a329493a77ec0e862fcce4671cd3768f32";
+    sha256 = "1lmmzb087ahmx2mdjarbi52a9424qczhzqbxrvcrg11cbmv9b191";
+  in import (builtins.fetchTarball {
+    url = "https://github.com/xtruder/kubenix/archive/${rev}.tar.gz";
+    inherit sha256;
+  }) { inherit pkgs; };
+
+  manifest = kubenix.buildResources {
+    configuration = import ./configuration.nix;
+  };
+
 in with pkgs; rec {
   shell = mkShell {
     buildInputs = [
       helloApp
       kind
       kubectl
+      deploy-to-kind
     ];
   };
 
@@ -23,4 +36,23 @@ in with pkgs; rec {
     tag = "latest";
     config.Cmd = [ "${helloApp}/bin/hello-app" ];
   };
+
+  deploy-to-kind = writeScriptBin "deploy-to-kind" ''
+    #! ${runtimeShell}
+    set -euo pipefail
+
+    ${kind}/bin/kind delete cluster || true
+    ${kind}/bin/kind create cluster
+
+    echo "Loading the docker image inside the kind docker container ..."
+    ( tmpfile=$(mktemp -t appImage.XXXXXX)
+      trap "rm -f '$tmpfile'" EXIT INT TERM
+      gzip --decompress --stdout ${appImage} > "$tmpfile"
+      kind load image-archive "$tmpfile"
+    )
+
+    echo "Applying the configuration ..."
+    ${jq}/bin/jq "." ${manifest}
+    ${kubectl}/bin/kubectl --context kind-kind apply -f ${manifest}
+  '';
 }
